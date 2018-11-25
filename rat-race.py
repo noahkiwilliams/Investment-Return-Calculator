bab6f736d49cf22d2d84e56647f7f5d8819d4759
swimjones <nwilliams17@punahou.edu>
2018-11-24 15:54:32 -1000

    add text features, inputs, restart ability

--- rat-race.py
+++ rat-race.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 ''' FORMULA TO DOLLAR COST AVERAGE A SUM EVERY YEAR INTO AN ASSET W/ X% RETURN '''
 ''' WELCOME TO THE RAT RACE... TIME TO TEST YOUR LUCK!!! '''
@@ -9,11 +10,10 @@ class rat(object):
         self.income = income
         self.rate = rate
         self.career = career
-
         ## create RAT object...
 
     def annual_savings(self):
-        annual_savings = self.income * self.rate
+        annual_savings = self.income * self.rate * .01
         # print(str(annual_savings))
         return(annual_savings)
 
@@ -25,23 +25,59 @@ class rat(object):
 def dollar_cost_average(rat, investment_returns):
     current_balance = rat.annual_savings()
 
-    for x in range (rat.career + 1):
+    for x in range (rat.career):
         a = x ## counter for year numbers
         x = round(current_balance, 3)
         rate_of_return = 1 + .01 * investment_returns
         new_balance = x * rate_of_return + rat.annual_savings()
         current_balance = new_balance
-        print("YEAR:" + str(a) + " = " + str(x)) ## print responses
+        print("YEAR:" + str(a + 1) + " = " + str(x)) ## print responses
 
-frank = rat("Frank", 150000, 0.1, 30) ## hypothetical RAT 1
-alice = rat("Alice", 150000, 0.1, 30) ## hypothetical RAT 1
+print('\n')
+print("WELCOME TO THE RAT RACE.")
+print("NOTHING IS GUARANTEED, PROFIT IS THE PURPOSE, AND YOU HAVE NO RIGHTS.")
+print("OBEDIENCE WILL BE REWARDED.")
 
-''' CREATE NEW RATS TO TEST DIFFERENT COMBINATIONS OF INCOME,
-RATE OF INVESTMENT RETURNS, YEARS WORKED, ETC. '''
+print('\n')
 
-print(str(frank.annual_savings))
-print(str(frank.lifetime_savings))
+print("THIS IS A CALCULATOR TO FIND THE RETURNS ON A PORTION OF INCOME DOLLAR-COST-AVERAGED INTO AN INVESTMENT WITH A CONSTANT ANNUAL RETURN.")
+print("DOLLAR-COST-AVERAGING IS A METHOD OF INVESTING A CONSTANT AMOUNT AT SET INTERVALS TO PREVENT CATASTROPHIC LOSSES.")
 
-dollar_cost_average(frank, 4)
-print("BREAK")
-dollar_cost_average(alice, 8)
+print('\n')
+print("*** DISCLAIMER: THESE RETURNS ARE THEORETICAL AND DO NOT REFLECT CURRENT FINANCIAL CONDITIONS. ***")
+print("*** DISCLAIMER: THIS IS NOT INVESTMENT ADVICE, AND NO ADVICE SHOULD BE TAKEN FROM THIS CALCULATION. ***")
+
+print('\n')
+
+def create_new_rat():
+    name = "RAT"
+    str_income = input("ENTER YOUR ANNUAL INCOME (NUMBERS ONLY): ")
+    str_rate = input("WHAT PERCENT OF YOUR INCOME DO YOU SAVE? ENTER A NUMBER BETWEEN 0 AND 100: ")
+    str_career = input("ENTER THE NUMBER OF YEARS YOU PLAN TO WORK: ")
+    str_investment_returns = input("WHAT PERCENT, ON AVERAGE, WILL YOUR INVESTMENTS RETURN? ENTER A NUMBER BETWEEN 0 AND 100: ")
+
+
+    income = int(str_income)
+    rate = int(str_rate)
+    career = int(str_career)
+    investment_returns = int(str_investment_returns)
+
+    new_rat = rat(name, income, rate, career)
+
+
+
+    print('\n')
+    print("YOUR DOLLAR-COST-AVERAGED RETURNS, YEAR-BY-YEAR:")
+    print('\n')
+
+    dollar_cost_average(new_rat, investment_returns)
+    print('\n')
+
+create_new_rat()
+
+print('\n')
+
+restart = input("ENTER 'NEW' TO MAKE A NEW CALCULATION: ")
+print('\n')
+if(restart == 'NEW' or restart == 'new'):
+    create_new_rat()
